bbd94969b1d1bff4f68544f2f8c58fe674fa87ec
Gennaro <gennarinodi02@gmail.com>
2022-04-01 14:15:41 +0200

    finito senza bonus

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,1 +1,20 @@
 ﻿
+string[] CibiPreferiti = { "Pizza", "patatine", "sushi", "pollo", "tonno", "salmone" };
+
+Console.WriteLine("i miei cibi preferiti sono:"+ CibiPreferiti.Length);
+Console.WriteLine();
+
+
+Console.WriteLine("questi sono i cibi preferiti :");
+
+for (int i = 0; i < CibiPreferiti.Length; i++)
+{
+    Console.WriteLine(CibiPreferiti[i]);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("il mio  cibo preferito TOP : " + CibiPreferiti[0]);
+
+Console.WriteLine();
+Console.WriteLine("il mio cibo meno preferito tra i preferiti è :" +CibiPreferiti[5]);
