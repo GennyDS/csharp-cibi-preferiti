46244fb953622597c3cc6b9409e55c84dc136a92
Gennaro <gennarinodi02@gmail.com>
2022-04-04 09:46:36 +0200

    Correzioni

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -1,6 +1,8 @@
 ﻿
-string[] CibiPreferiti = { "Pizza", "patatine", "sushi", "pollo", "tonno", "salmone" };
- int h= CibiPreferiti.Length/2;
+string[] CibiPreferiti = { "Pizza", "patatine", "sushi", "pollo", "tonno","salmone" };
+ int mediana= CibiPreferiti.Length/2;
+
+
 Console.WriteLine();
 
 Console.WriteLine("i miei cibi preferiti sono:" + CibiPreferiti.Length);
@@ -11,6 +13,9 @@ Console.WriteLine("questi sono i cibi preferiti :");
 
 for (int i = 0; i < CibiPreferiti.Length; i++)
 {
+    Console.Write(i + 1);
+    Console.Write(":");
+    Console.Write("  ");
     Console.WriteLine(CibiPreferiti[i]);
 }
 
@@ -24,8 +29,18 @@ Console.WriteLine("il mio cibo meno preferito tra i preferiti è :" + CibiPrefer
 
 Console.WriteLine();
 
+if(CibiPreferiti.Length % 2 == 0)
+{
+    Console.WriteLine("il cibo di metà classifica è :");
+        Console.WriteLine(CibiPreferiti[mediana - 1]);
+    Console.WriteLine(CibiPreferiti[mediana]);
+    
+}
+else
+{
+    Console.WriteLine("il cibo di metà classifica è :" + CibiPreferiti[mediana]);
 
-Console.WriteLine("il cibo di metà classifica è :"+CibiPreferiti[h]);
+}
 
 
 
